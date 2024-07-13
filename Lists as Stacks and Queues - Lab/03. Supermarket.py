ad254342d6f409831aa1ddb3adf5a552cf7c8b02
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-07-14 00:58:50 +0300

    Create 03. Supermarket.py

---
+++ Lists as Stacks and Queues - Lab/03. Supermarket.py
@@ -0,0 +1,13 @@
+from collections import deque
+my_deque = deque()
+while True:
+    name = input()
+    if name == 'End':
+        break
+    elif name == 'Paid':
+        for i in range(len(my_deque)):
+            print(my_deque.popleft())
+    else:
+        my_deque.append(name)
+
+print(f"{len(my_deque)} people remaining.")
