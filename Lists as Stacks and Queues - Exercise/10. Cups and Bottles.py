e3b26ee1e526812ee84a3120d81f7827c85bc539
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-07-16 22:53:06 +0300

    Create 10. Cups and Bottles.py

---
+++ Lists as Stacks and Queues - Exercise/10. Cups and Bottles.py
@@ -0,0 +1,20 @@
+from collections import deque
+
+cups = deque(int(n) for n in input().split())
+bottles = list(int(n) for n in input().split())
+wasted_water = 0
+
+while bottles and cups:
+    water = bottles.pop() - cups.popleft()
+    if water >= 0:
+        wasted_water += water
+    else:
+        cups.appendleft(abs(water))
+
+if not cups:
+    print('Bottles: ', end='')
+    print(*bottles)
+else:
+    print('Cups: ', end='')
+    print(*cups)
+print(f"Wasted litters of water: {wasted_water}")
