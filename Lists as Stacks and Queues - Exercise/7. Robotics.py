fa768d228605f24db85ad3eb1dac5bf554a6d980
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-07-17 00:00:36 +0300

    Update 7. Robotics.py

--- Lists as Stacks and Queues - Exercise/7. Robotics.py
+++ Lists as Stacks and Queues - Exercise/7. Robotics.py
@@ -44,33 +44,71 @@
 
 #second solution:
 
+# from collections import deque
+# from datetime import datetime, timedelta
+#
+# robots = deque(r.split('-') for r in input().split(';'))
+# time = datetime.strptime(input(), '%H:%M:%S')
+# details = deque()
+#
+# while True:
+#     detail = input()
+#     if detail == 'End':
+#         break
+#     details.append(detail)
+#
+# working_robots = {}
+#
+# while details:
+#     time += timedelta(seconds=1)
+#     current_detail = details.popleft()
+#     if len(robots) > 0:
+#         current_robot = robots.popleft()
+#         print(f"{current_robot[0]} - {current_detail} [{time.strftime('%H:%M:%S')}]")
+#         working_robots[(current_robot[0], current_robot[1])] = int(current_robot[1])
+#     else:
+#         details.append(current_detail)
+#     copied_working_robots = working_robots.copy()
+#     for k, v in copied_working_robots.items():
+#         working_robots[k] -= 1
+#         if working_robots[k] == 0:
+#             robots.append(k)
+#             del working_robots[k]
+
+
+#third solution -> 100/100 in judge
+
 from collections import deque
 from datetime import datetime, timedelta
 
-robots = deque(r.split('-') for r in input().split(';'))
+robots = {}
+
+for r in input().split(';'):
+    name, time = r.split('-')
+    robots[name] = [int(time), 0]
+
 time = datetime.strptime(input(), '%H:%M:%S')
-details = deque()
+product = deque()
 
 while True:
-    detail = input()
-    if detail == 'End':
+    command = input()
+    if command == 'End':
         break
-    details.append(detail)
+    product.append(command)
 
-working_robots = {}
-
-while details:
+while product:
     time += timedelta(seconds=1)
-    current_detail = details.popleft()
-    if len(robots) > 0:
-        current_robot = robots.popleft()
-        print(f"{current_robot[0]} - {current_detail} [{time.strftime('%H:%M:%S')}]")
-        working_robots[(current_robot[0], current_robot[1])] = int(current_robot[1])
+    current_product = product.popleft()
+    free_robots = deque()
+    for k, v in robots.items():
+        if robots[k][1] == 0:
+            free_robots.append(k)
+    if free_robots:
+        robot_name = free_robots.popleft()
+        robots[robot_name][1] = robots[robot_name][0]
+        print(f"{robot_name} - {current_product} [{time.strftime('%H:%M:%S')}]")
     else:
-        details.append(current_detail)
-    copied_working_robots = working_robots.copy()
-    for k, v in copied_working_robots.items():
-        working_robots[k] -= 1
-        if working_robots[k] == 0:
-            robots.append(k)
-            del working_robots[k]
+        product.append(current_product)
+    for k, v in robots.items():
+        if robots[k][1] != 0:
+            robots[k][1] -= 1
