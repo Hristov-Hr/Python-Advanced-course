f3083d6a961e0bca6c120ca1625c0dcc4deac68e
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-07-21 23:15:35 +0300

    Update 1. Unique Usernames.py

--- Tuples and Sets - Exercise/1. Unique Usernames.py
+++ Tuples and Sets - Exercise/1. Unique Usernames.py
@@ -4,3 +4,6 @@ for _ in range(int(input())):
     names.add(input())
 for name in names:
     print(name)
+
+# solution 2 -> set - comprehension
+# print(*{input() for _ in range(int(input()))}, sep='\n')
