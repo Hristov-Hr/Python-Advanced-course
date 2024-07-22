880ce7727e63e5dfc628becc8cbbcc211c9ba895
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-07-23 00:23:31 +0300

    Update 6. Paint Colors.py

--- Stacks, Queues, Tuples and Sets - Exercise/6. Paint Colors.py
+++ Stacks, Queues, Tuples and Sets - Exercise/6. Paint Colors.py
@@ -1,9 +1,31 @@
 from collections import deque
 
-main_colors = ("red", "yellow", "blue")
+colors = ("red", "yellow", "blue", "orange", "purple", "green")
+secondary_colors = {"orange": ['red', 'yellow'], "purple": ['red', 'blue'], "green": ['yellow', 'blue']}
+collected_colors = []
 
 substrings_sequence = deque(input().split())
 
 while substrings_sequence:
     first_substring = substrings_sequence.popleft()
-    second_substring = substrings_sequence.pop()
+    if substrings_sequence:
+        second_substring = substrings_sequence.pop()
+    else:
+        second_substring = ''
+
+    for color in colors:
+        if first_substring + second_substring == color or second_substring + first_substring == color:
+            collected_colors.append(color)
+            break
+    else:
+        if first_substring[:-1] != '':
+            substrings_sequence.insert(len(substrings_sequence) // 2, first_substring[:-1])
+        if second_substring[:-1] != '':
+            substrings_sequence.insert(len(substrings_sequence) // 2, second_substring[:-1])
+
+for color in collected_colors:
+    if color in secondary_colors:
+        if not set(secondary_colors[color]).issubset(collected_colors):
+            collected_colors.remove(color)
+
+print(collected_colors)
