0df2e4792dcba07230d2ba62832cf8f0c049fc70
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-07-23 00:23:27 +0300

    Update 3. Milkshakes.py

--- Stacks, Queues, Tuples and Sets - Exercise/3. Milkshakes.py
+++ Stacks, Queues, Tuples and Sets - Exercise/3. Milkshakes.py
@@ -1,36 +1,32 @@
 from collections import deque
 
-chocolate_numbers = deque(int(x) for x in input().split(', '))
+chocolates = deque(int(x) for x in input().split(', '))
 milk_cups = deque(int(x) for x in input().split(', '))
 milkshakes = 0
 
 while milkshakes < 5:
-    if not chocolate_numbers or not milk_cups:
+    if not chocolates or not milk_cups:
         break
-    chocolate = chocolate_numbers.pop()
+    chocolate = chocolates.pop()
     milk = milk_cups.popleft()
-    if chocolate <= 0 or milk <= 0:
-        if chocolate > 0:
-            chocolate_numbers.append(chocolate)
-        if milk > 0:
-            milk_cups.appendleft(milk)
+    if chocolate <= 0 and milk <= 0:
+        continue
+    elif chocolate <= 0:
+        milk_cups.appendleft(milk)
+        continue
+    elif milk <= 0:
+        chocolates.append(chocolate)
         continue
 
     if chocolate == milk:
         milkshakes += 1
     else:
         milk_cups.append(milk)
-        chocolate_numbers.appendleft(chocolate - 5)
+        chocolates.append(chocolate - 5)
 
 if milkshakes == 5:
     print('Great! You made all the chocolate milkshakes needed!')
 else:
     print('Not enough milkshakes.')
-if len(chocolate_numbers) > 0:
-    print(f"Chocolate: {', '.join(str(x) for x in chocolate_numbers)}")
-else:
-    print('Chocolate: empty')
-if len(milk_cups) > 0:
-    print(f"Milk: {', '.join(str(x) for x in milk_cups)}")
-else:
-    print('Milk: empty')
+print(f"Chocolate: {', '.join(str(x) for x in chocolates) or 'empty'}")
+print(f"Milk: {', '.join(str(x) for x in milk_cups) or 'empty'}")
