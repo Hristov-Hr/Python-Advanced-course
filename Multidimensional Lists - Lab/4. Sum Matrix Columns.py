033703f71acdcce36916cd991923771d430e0cf4
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-07-24 22:13:41 +0300

    Update 4. Sum Matrix Columns.py

--- Multidimensional Lists - Lab/4. Sum Matrix Columns.py
+++ Multidimensional Lists - Lab/4. Sum Matrix Columns.py
@@ -1,2 +1,8 @@
 rows, columns = input().split(', ')
+matrix = [0 for _ in range(int(columns))]
 
+for _ in range(int(rows)):
+    sublist = [int(x) for x in input().split()]
+    for j in range(int(columns)):
+        matrix[j] += sublist[j]
+print(*matrix, sep='\n')
