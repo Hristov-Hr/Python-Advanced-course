fb6ed90a6eeec85fc01aa796d50418a896c65774
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-07-25 23:55:10 +0300

    Create 3. 2x2 Squares in Matrix.py

---
+++ Multidimensional Lists - Exercise 1/3. 2x2 Squares in Matrix.py
@@ -0,0 +1,10 @@
+rows, columns = [int(x) for x in input().split()]
+matrix = [[x for x in input().split()] for _ in range(rows)]
+equal_cells = 0
+
+for i in range(rows - 1):
+    for j in range(columns - 1):
+        if matrix[i][j] == matrix[i][j + 1] == matrix[i + 1][j] == matrix[i + 1][j + 1]:
+            equal_cells += 1
+
+print(equal_cells)
