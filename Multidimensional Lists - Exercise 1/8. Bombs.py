42c29f3e2dcb1608990ed4bef20f2f8b6ee78452
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-07-27 15:59:14 +0300

    Create 8. Bombs.py

---
+++ Multidimensional Lists - Exercise 1/8. Bombs.py
@@ -0,0 +1,16 @@
+matrix = [[int(n) for n in input().split()] for _ in range(int(input()))]
+coordinates = [[int(y) for y in x.split(',')] for x in input().split()]
+
+for n in coordinates:
+    row, column = n
+    value = matrix[row][column]
+    if value <= 0:
+        continue
+    for r in range(row - 1, row + 2):
+        for c in range(column - 1, column + 2):
+            if 0 <= r < len(matrix) and 0 <= c < len(matrix) and matrix[r][c] > 0:
+                matrix[r][c] -= value
+
+print(f"Alive cells: {len([n for row in range(len(matrix)) for n in matrix[row] if n > 0])}")
+print(f"Sum: {sum([n for row in range(len(matrix)) for n in matrix[row] if n > 0])}")
+[print(*sub) for sub in matrix]
