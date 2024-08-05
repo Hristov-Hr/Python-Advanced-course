cf3f89b250e024c821372d0f546314df907177d8
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-08-05 23:38:19 +0300

    Update 4. Numbers Filter.py

--- Functions Advanced - Exercise/4. Numbers Filter.py
+++ Functions Advanced - Exercise/4. Numbers Filter.py
@@ -4,12 +4,24 @@ def even_odd_filter(**kwargs):
         'odd': lambda x: x % 2 != 0
     }
 
+    result = {}
+
     for k, v in kwargs.items():
         kwargs[k] = [*filter(filters[k], v)]
-    print(kwargs)
+
+    kwargs = sorted(kwargs.items(), key=lambda x: len(x[1]), reverse=True)
+
+    for el in kwargs:
+        result[el[0]] = el[1]
+
+    return result
 
 
-print(even_odd_filter(
-    odd=[1, 2, 3, 4, 10, 5],
-    even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
-))
+# print(even_odd_filter(
+#     odd=[1, 2, 3, 4, 10, 5],
+#     even=[3, 4, 5, 7, 10, 2, 5, 5, 2],
+# ))
+# print(even_odd_filter(
+#     odd=[2, 2, 30, 44, 10, 5],
+# ))
+
