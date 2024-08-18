450f671e6c9d11b3a05c99a1dc40d9cd8a5a373f
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-08-18 16:21:02 +0300

    Update 5. Longest Intersection.py

--- Tuples and Sets - Exercise/5. Longest Intersection.py
+++ Tuples and Sets - Exercise/5. Longest Intersection.py
@@ -9,10 +9,14 @@ for _ in range(int(input())):
 
     for n in range(int(first_start), int(first_end) + 1):
         first_set.add(n)
+
     for n in range(int(second_start), int(second_end) + 1):
         second_set.add(n)
+
     intersection = first_set & second_set
+
     if len(intersection) > longest_intersection['length']:
         longest_intersection['length'] = len(intersection)
         longest_intersection['numbers'] = list(intersection)
+
 print(f"Longest intersection is {longest_intersection['numbers']} with length {longest_intersection['length']}")
