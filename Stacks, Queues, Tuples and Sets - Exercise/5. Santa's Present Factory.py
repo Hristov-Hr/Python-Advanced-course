f2270edcc255a5d9e4e3fb84723bae38eb6a01d5
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-09-16 00:43:38 +0300

    Update 5. Santa's Present Factory.py

--- Stacks, Queues, Tuples and Sets - Exercise/5. Santa's Present Factory.py
+++ Stacks, Queues, Tuples and Sets - Exercise/5. Santa's Present Factory.py
@@ -1,45 +1,47 @@
 from collections import deque
 
-presents_magic_values = {
-    'Doll': 150,
-    'Wooden train': 250,
-    'Teddy bear': 300,
-    'Bicycle': 400
+
+presents_values = {
+    150: 'Doll',
+    250: 'Wooden train',
+    300: 'Teddy bear',
+    400: 'Bicycle'
 }
+
 crafted_presents = {}
 
-materials = [int(x) for x in input().split()]
-magic = deque(int(x) for x in input().split())
+materials_values = deque(int(x) for x in input().split())
+magic_values = deque(int(x) for x in input().split())
 
-while materials and magic:
-    current_material = materials.pop()
-    current_magic = magic.popleft()
-    magic_level = current_material * current_magic
-    for gift, value in presents_magic_values.items():
-        if value == magic_level:
-            if gift not in crafted_presents:
-                crafted_presents[gift] = 0
-            crafted_presents[gift] += 1
-            break
-    else:
-        if magic_level < 0:
-            materials.append(current_material + current_magic)
-        elif magic_level == 0:
-            if current_material != 0:
-                materials.append(current_material)
-            elif current_magic != 0:
-                magic.appendleft(current_magic)
-        else:
-            materials.append(current_material + 15)
+while magic_values and materials_values:
 
-if {'Doll', 'Wooden train'}.issubset(crafted_presents) or {'Teddy bear', 'Bicycle'}.issubset(crafted_presents):
+    material = materials_values.pop()
+    magic = magic_values.popleft()
+    if magic == 0 or material == 0:
+        if magic != 0:
+            magic_values.appendleft(magic)
+        if material != 0:
+            materials_values.append(material)
+        continue
+
+    magic_level = magic * material
+    present = presents_values.get(magic_level, None)
+
+    if present:
+        if present not in crafted_presents:
+            crafted_presents[present] = 0
+        crafted_presents[present] += 1
+        continue
+
+    material += magic if magic_level < 0 else 15
+    materials_values.append(material)
+
+
+if {"Wooden train", "Doll"}.issubset(crafted_presents) or {'Teddy bear', 'Bicycle'}.issubset(crafted_presents):
     print("The presents are crafted! Merry Christmas!")
 else:
     print("No presents this Christmas!")
-if materials:
-    print(f"Materials left: {', '.join(str(x) for x in reversed(materials))}")
-if magic:
-    print(f"Magic left: {', '.join(str(x) for x in magic)}")
 
-for gift, amount in sorted(crafted_presents.items()):
-    print(f"{gift}: {amount}")
+print(f"Materials left: {', '.join(str(x) for x in reversed(materials_values))}") if materials_values else None
+print(f"Magic left: {', '.join(str(x) for x in magic_values)}") if magic_values else None
+[print(f"{k}: {v}") for k, v in sorted(crafted_presents.items())]
