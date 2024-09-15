e7db18dbcf19f7264ae9d06786cd4b9138551c2b
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-09-15 23:30:45 +0300

    Update 4. Honey.py

--- Stacks, Queues, Tuples and Sets - Exercise/4. Honey.py
+++ Stacks, Queues, Tuples and Sets - Exercise/4. Honey.py
@@ -1,30 +1,34 @@
 from collections import deque
 
-functions = {
-    '-': lambda a, b: abs(a - b),
-    '+': lambda a, b: abs(a + b),
-    '*': lambda a, b: abs(a * b),
-    '/': lambda a, b: abs(a / b) if b != 0 else 0,
-}
+
+def honey_making_process(bee_, ntr, sym):
+
+    calc = {
+        "-": lambda x, y: x - y,
+        "+": lambda x, y: x + y,
+        "*": lambda x, y: x * y,
+        "/": lambda x, y: x / y if y != 0 else 0
+    }
+
+    return abs(calc[sym](bee_, ntr))
+
 
 bees = deque(int(x) for x in input().split())
-nectar_values = [int(x) for x in input().split()]
+nectar_values = deque(int(x) for x in input().split())
 symbols = deque(input().split())
-collected_honey = 0
+honey = 0
 
 while bees and nectar_values:
+
     bee = bees.popleft()
     nectar = nectar_values.pop()
-    symbol = symbols.popleft()
+
     if bee > nectar:
         bees.appendleft(bee)
-        symbols.appendleft(symbol)
-    else:
-        honey = functions[symbol](bee, nectar)
-        collected_honey += honey
+        continue
 
-print(f"Total honey made: {collected_honey}")
-if bees:
-    print(f"Bees left: {', '.join(str(x) for x in bees)}")
-if nectar_values:
-    print(f"Nectar left: {', '.join(str(x) for x in nectar_values)}")
+    honey += honey_making_process(bee, nectar, symbols.popleft())
+
+print(f"Total honey made: {honey}")
+print(f"Bees left: {', '.join(str(x) for x in bees)}") if bees else None
+print(f"Nectar left: {', '.join(str(x) for x in nectar_values)}") if nectar_values else None
