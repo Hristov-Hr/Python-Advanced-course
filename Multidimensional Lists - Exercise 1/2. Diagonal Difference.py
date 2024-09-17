b10b4dab8ef2bfcddfaa74cef483b89a5d4caf1c
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-09-17 22:14:17 +0300

    Update 2. Diagonal Difference.py

--- Multidimensional Lists - Exercise 1/2. Diagonal Difference.py
+++ Multidimensional Lists - Exercise 1/2. Diagonal Difference.py
@@ -1,13 +1,12 @@
-matrix = [[int(x) for x in input().split()] for _ in range(int(input()))]
-
+SIZE = int(input())
+matrix = []
 primary_diagonal = []
 secondary_diagonal = []
 
-for i in range(len(matrix)):
-    for j in range(len(matrix)):
-        if i == j:
-            primary_diagonal.append(matrix[i][i])
-        if j == len(matrix) - 1 - i:
-            secondary_diagonal.append(matrix[i][j])
+for i in range(SIZE):
+    row = [int(x) for x in input().split()]
+    matrix.append(row)
+    primary_diagonal.append(matrix[i][i])
+    secondary_diagonal.append(matrix[i][SIZE - 1 - i])
 
 print(abs(sum(primary_diagonal) - sum(secondary_diagonal)))
