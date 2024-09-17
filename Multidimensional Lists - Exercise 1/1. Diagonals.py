4366f504482990207747ae66f8096ad659cc98ec
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-09-17 22:14:20 +0300

    Update 1. Diagonals.py

--- Multidimensional Lists - Exercise 1/1. Diagonals.py
+++ Multidimensional Lists - Exercise 1/1. Diagonals.py
@@ -1,15 +1,13 @@
-matrix = [[int(x) for x in input().split(', ')] for _ in range(int(input()))]
-
+SIZE = int(input())
+matrix = []
 primary_diagonal = []
 secondary_diagonal = []
 
-for i in range(len(matrix)):
-    for j in range(len(matrix)):
-        if i == j:
-            primary_diagonal.append(matrix[i][i])
-        if j == len(matrix) - 1 - i:
-            secondary_diagonal.append(matrix[i][j])
+for i in range(SIZE):
+    row = [int(x) for x in input().split(', ')]
+    matrix.append(row)
+    primary_diagonal.append(matrix[i][i])
+    secondary_diagonal.append(matrix[i][SIZE - 1 - i])
 
-
-print(f"Primary diagonal: {', '.join([str(x) for x in primary_diagonal])}. Sum: {sum(primary_diagonal)}")
-print(f"Secondary diagonal: {', '.join([str(x) for x in secondary_diagonal])}. Sum: {sum(secondary_diagonal)}")
+print(f"Primary diagonal: {', '.join(str(n) for n in primary_diagonal)}. Sum: {sum(primary_diagonal)}")
+print(f"Secondary diagonal: {', '.join(str(n) for n in secondary_diagonal)}. Sum: {sum(secondary_diagonal)}")
