9687fde84a4ea12962c35c0c6c3afc5a95611f25
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-09-22 15:54:26 +0300

    Update 7. Snake Moves.py

--- Multidimensional Lists - Exercise 1/7. Snake Moves.py
+++ Multidimensional Lists - Exercise 1/7. Snake Moves.py
@@ -6,9 +6,8 @@ snake = deque([x for x in input()])
 for i in range(rows):
     sub_matrix = ''
     for j in range(columns):
-        char = snake.popleft()
-        snake.append(char)
-        sub_matrix += char
+        sub_matrix += snake[0]
+        snake.rotate(-1)
     if i % 2 != 0:
         sub_matrix = sub_matrix[::-1]
     print(sub_matrix)
