58ce3100e5f1e50cbc18ba181a9ba1a0c34e33b5
Hristov-Hr <169080766+Hristov-Hr@users.noreply.github.com>
2024-09-22 15:54:24 +0300

    Update 6. Matrix Shuffling.py

--- Multidimensional Lists - Exercise 1/6. Matrix Shuffling.py
+++ Multidimensional Lists - Exercise 1/6. Matrix Shuffling.py
@@ -1,18 +1,20 @@
-rows, columns = [int(x) for x in input().split()]
-matrix = [input().split() for _ in range(rows)]
+row, col = [int(i) for i in input().split()]
+matrix = [input().split() for _ in range(row)]
 
-while True:
-    command, *data = input().split()
-    if command == 'END':
-        break
-    if command != 'swap' or len(data) != 4:
-        print('Invalid input!')
-        continue
-    row_1, col_1, row_2, col_2 = [int(x) for x in data]
-    if 0 <= row_1 < rows and 0 <= row_2 < rows and 0 <= col_1 < columns and 0 <= col_2 < columns:
-        matrix[row_1][col_1], matrix[row_2][col_2] = matrix[row_2][col_2], matrix[row_1][col_1]
-        for row in matrix:
-            print(*row)
-    else:
-        print('Invalid input!')
-        continue
+command = input()
+
+while command != 'END':
+
+    try:
+        r_1, c_1, r_2, c_2 = [int(n) for n in command.split()[1:]]
+        try:
+            matrix[r_1][c_1], matrix[r_2][c_2] = matrix[r_2][c_2], matrix[r_1][c_1]
+            [print(' '.join(m)) for m in matrix]
+
+        except IndexError:
+            print("Invalid input!")
+
+    except ValueError:
+        print("Invalid input!")
+
+    command = input()
